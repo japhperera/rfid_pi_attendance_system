7516b779d072e7a207ca7252cb24cc14950f0bd4
japh.perera <japh.perera@gmai.com>
2018-10-08 19:17:37 +0100

    Student Table Changes

--- Write.py
+++ Write.py
@@ -2,6 +2,26 @@
 
 import RPi.GPIO as GPIO
 import SimpleMFRC522
+import mysql.connector
+
+def update_db(name, tag):
+	mydb = mysql.connector.connect(
+ 			host="localhost",
+  			user="pi",
+  			passwd="pi@123",
+ 			database="AT_SYS"
+	)
+
+	mycursor = mydb.cursor()
+	insert_query = "INSERT INTO students (name, tag) VALUES (%s, %s)"	
+	val = (name, tag)
+
+	mycursor.execute(insert_query,val)
+	mydb.commit()
+	print("1 record inserted, ID:", mycursor.lastrowid)
+
+
+
 
 reader = SimpleMFRC522.SimpleMFRC522()
 
@@ -10,5 +30,6 @@ try:
         print("Now place your tag to write")
         reader.write(text)
         print("Written")
+	update_db(text, "New Student")
 finally:
         GPIO.cleanup()
