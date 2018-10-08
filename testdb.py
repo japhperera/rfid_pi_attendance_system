3d6764212bdc6fd3b9cbc28aaed91fa2db276581
japh.perera <japh.perera@gmai.com>
2018-10-08 18:28:39 +0100

    Db connection test scripts

--- testdb.py
+++ testdb.py
@@ -5,12 +5,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="pi",
-  passwd="pi@123"
+  passwd="pi@123",
+ database="AT_SYS"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SHOW DATABASES")
+create_query = "CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),tag VARCHAR(255))"
+
+mycursor.execute(create_query)
 
 for x in mycursor:
   print(x)
