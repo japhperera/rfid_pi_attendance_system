2090ebced392edae7787023423bf3b0e9694a0ae
japh.perera <japh.perera@gmai.com>
2018-10-08 19:05:44 +0100

    attendance

--- Read.py
+++ Read.py
@@ -2,7 +2,9 @@
 
 import RPi.GPIO as GPIO
 import SimpleMFRC522
-import time 
+import time
+import mysql.connector
+import datetime
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -18,7 +20,30 @@ def get_data():
         id, text = reader.read()
         print(id)
         print(text)
+	update_db(text, id)
 	blink_led()
+
+
+def update_db(name, tag):
+	mydb = mysql.connector.connect(
+ 			host="localhost",
+  			user="pi",
+  			passwd="pi@123",
+ 			database="AT_SYS"
+	)
+
+	mycursor = mydb.cursor()
+	insert_query = "INSERT INTO attendance (name, tag,date,time) VALUES (%s, %s,%s, %s)"
+	now = datetime.datetime.now()
+	cr_date = now.strftime("%Y-%m-%d")
+	cr_time = now.strftime("%H:%M")	
+	val = (name, tag, cr_date, cr_time )
+
+	mycursor.execute(insert_query,val)
+	mydb.commit()
+	print("1 record inserted, ID:", mycursor.lastrowid)
+
+
 
 
 
@@ -27,4 +52,9 @@ reader = SimpleMFRC522.SimpleMFRC522()
 while True:
 	print "Waiting for a tag ...."
 	get_data()
-rr
+
+
+
+
+
+
--- testdb.py
+++ testdb.py
@@ -11,7 +11,8 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-create_query = "CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),tag VARCHAR(255))"
+#create_query = "CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),tag VARCHAR(255))"
+create_query = "CREATE TABLE attendance (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),tag VARCHAR(255),date VARCHAR(20), time VARCHAR(10))"
 
 mycursor.execute(create_query)
 
